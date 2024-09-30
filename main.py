0647a5ed23c4f8e74abb8a8566e4dc13c07b6829
Makar <makarmihailovi4@mail.ru>
2024-09-30 13:12:12 +0300

    second commit

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import json
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-# Opens the file in read-only mode and assigns the contents to the variable cfg to be accessed further down
-with open('config.json', 'r') as cfg:
-  # Deserialize the JSON data (essentially turning it into a Python dictionary object so we can use it in our code) 
+# открытие конфиг файла с токеном
+with open('config.txt', 'r') as cfg:
+  # получение токена бота
   data = json.load(cfg) 
 
 async def start(update: Update, context):
-    await update.message.reply_text("Приветик! Я твой бот для астрологических предсказаний или же гороскопов :) /n Для начала я должен узнать твой день и месяц рождения!)")
+    await update.message.reply_text("Приветик! Я твой бот для астрологических предсказаний или же гороскопов :) Для начала работы я должен узнать твой день и месяц рождения!)")
 
 if __name__ == '__main__':
     app = ApplicationBuilder().token(data["token"]).build() #создание и вызов экземпляра бота
